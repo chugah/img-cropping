5a0814c10719c3dd1af3461a3190d08b0311fc02
Sami Barghshoon <SBarghshoon@spi.local>
2018-02-20 12:38:32 -0500

    Added crop logic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 (function() {
+  // var needed for image cropping
   var el, lastActive, object;
 
+  // var needed to redo/undo cropping
   var config = {
     canvasState             : [],
     currentStateIndex       : -1,
@@ -10,16 +12,18 @@
     redoFinishedStatus      : 1
   };
 
+  // bounding box characteristics
   fabric.Object.prototype.transparentCorners = false;
-  var $ = function(id){return document.getElementById(id)};
-
   fabric.Object.prototype.padding = 5;
   fabric.Object.prototype.transparentCorners = false;
 
+  //jquery handler
+  var $ = function(id){return document.getElementById(id)};
+
+  // canvas set-up
   var canvas = this.__canvas = new fabric.Canvas('c');
   canvas.backgroundColor = '#ffffff';
   canvas.on({
-
      'object:modified': function () {
       updateModifications();  
     },
@@ -29,6 +33,7 @@
     }
   });
 
+  //upload images
   fabric.Image.fromURL(document.getElementById('gingerbread-house').src, function(img) {
     var oImg = img.set({ left: 25, top: 50, angle: 0, id: 'gingerbread-house' }).scale(0.9);
     canvas.add(oImg).renderAll();
@@ -39,22 +44,17 @@
     var oImg = img.set({ left: 300, top: 300, angle: 0  }).scale(0.9);
     canvas.add(oImg).renderAll();
     canvas.setActiveObject(oImg);
-
   });
 
-  // crop image
+  // crop image handlers
   $('startCropRect').onclick = function(){ startCropRect(); };
-
   $('endCropRect').onclick = function(){ endCropRect(); };
-
   $('startCropEl').onclick = function(){ startCropEl(); };
-
   $('endCropEl').onclick = function(){ endCropEl(); };
-
   $('undoCrop').onclick = function(){ undoCrop(); };
-
   $('redoCrop').onclick = function(){ redoCrop(); };
 
+  // initiate crop function with rectangle
   function startCropRect(){
     canvas.remove(el);
     if(canvas.getActiveObject()) {  
@@ -91,8 +91,8 @@
   function endCropRect(){
     var left = el.left - object.left;
     var top = el.top - object.top;    
-    var width = el.width * 1;
-    var height = el.height * 1;
+    var width = el.width;
+    var height = el.height;
     object.clipTo = function (ctx) {      
       ctx.rect(-(width/2)+left, -(height/2)+top, parseInt(width*el.scaleX), parseInt(height*el.scaleY));
     }   
@@ -101,6 +101,7 @@
     canvas.renderAll();   
   }
 
+  // initiate crop function with circle
   function startCropEl(){
     canvas.remove(el);
     if(canvas.getActiveObject()) {  
@@ -146,7 +147,7 @@
     console.log('height before ', height);
     console.log('object width ', objectWidth);
     object.clipTo = function (ctx) { 
-      if (top < 0 || left < 0) {
+      if (top < 0 || left < 0 || height*el.scaleY > objectWidth) {
         console.log('left < 0 ', left);
         console.log('top < 0 ', top);
         console.log('width ', width);
@@ -159,12 +160,16 @@
         console.log('top > 0 ', top);
         console.log('width ', parseInt(width*el.scaleX));
         console.log('height ', parseInt(height*el.scaleY));
+        console.log('el.scaleX ', el.scaleX);
+        console.log('el.scaleY ', el.scaleY);
       } else {
         ctx.ellipse(left, top, parseInt(width*el.scaleX), parseInt(height*el.scaleY), 45 * Math.PI/180, 0, 2 * Math.PI);
         console.log('left = 0 ', left);
         console.log('top = 0 ', top);
         console.log('width ', parseInt(width*el.scaleX));
         console.log('height ', parseInt(height*el.scaleY));
+        console.log('el.scaleX ', el.scaleX);
+        console.log('el.scaleY ', el.scaleY);
       }  
     }
     canvas.remove(canvas.getActiveObject(el));
@@ -172,6 +177,7 @@
     canvas.renderAll();   
   }
 
+  // undo redo state change function
   var updateModifications = function() {
     if((config.undoStatus == false && config.redoStatus == false)){
       var jsonData = canvas.toJSON();
@@ -188,6 +194,7 @@
      }
   }
  
+  // undo crop function
   var undoCrop = function() {
     if(config.undoFinishedStatus){
       if(config.currentStateIndex == -1){
@@ -216,6 +223,7 @@
     }
   }
 
+  // redo crop function 
   var redoCrop = function() {
     if(config.redoFinishedStatus){
       if (config.canvasState.length > config.currentStateIndex && config.canvasState.length != 0){
